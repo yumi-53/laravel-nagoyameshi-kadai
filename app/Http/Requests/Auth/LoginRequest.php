b1f17155004cedecf30f6a61e3f6f6ec0d7cbfab
yumi-53 <yk53713@gmail.com>
2024-09-12 11:35:02 +0900

    管理者側の認証を実装

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -40,8 +40,9 @@ class LoginRequest extends FormRequest
     public function authenticate(): void
     {
         $this->ensureIsNotRateLimited();
+        $this->is('admin/*') ? $guard = 'admin' : $guard = 'web';
 
-        if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
+        if (! Auth::guard($guard)->attempt($this->only('email', 'password'), $this->boolean('remember')))  {
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Admin;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Admin::create([
+            'email' => 'admin@example.com',
+            'password' => Hash::make('nagoyameshi'),
+        ]);
+    }
+}
