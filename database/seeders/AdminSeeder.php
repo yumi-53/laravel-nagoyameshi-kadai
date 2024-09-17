50d6122739d2207eae4c467b7c17b8d98656cfe8
yumi-53 <yk53713@gmail.com>
2024-09-18 06:06:53 +0900

    AdminSeeder変更

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -14,14 +14,14 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
-        Admin::firstOrCreate([
-            'email' => 'admin@example.com',
-            'password' => Hash::make('nagoyameshi'),
-        ]);
+        Admin::firstOrCreate(
+            ['email' => 'admin@example.com'],
+            ['password' => Hash::make('nagoyameshi')],
+        );
 
-        Admin::firstOrCreate([
-            'email' => 'samurai_kadai@example.com',
-            'password' => Hash::make('kadaiteishutu'),
-        ]);
+        Admin::firstOrCreate(
+            ['email' => 'samurai_kadai@example.com'],
+            ['password' => Hash::make('kadaiteishutu')],
+        );
     }
 }
