81955125ce6d6369d917df07cb0601f72c3ee256
yumi-53 <yk53713@gmail.com>
2024-09-26 15:48:10 +0900

    課題提出：管理側のカテゴリ管理機能を作ろう

--- app/Http/Controllers/Admin/RestaurantController.php
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -1,9 +1,9 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
 use App\Models\Restaurant;
 
 class RestaurantController extends Controller
