5d7fac793dfa2020607c4b5d41817149a3a40990
yumi-53 <yk53713@gmail.com>
2024-10-10 23:47:26 +0900

    課題提出：管理者側のトップページを作ろう

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,10 +4,22 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
+use App\Models\Restaurant;
+use App\Models\Reservation;
 
 class HomeController extends Controller
 {
     public function index() {
-        return view('admin.home');
+        $total_users = User::count();
+        $total_premium_users = DB::table('subscriptions')->where('stripe_status','=','active')->count();
+        $total_free_users = $total_users - $total_premium_users;
+        $total_restaurants = Restaurant::count();
+        $total_reservations = Reservation::count();
+        $sales_for_this_month = $total_premium_users * 300;
+
+        $returnItem = ['total_users','total_premium_users','total_free_users','total_restaurants','total_reservations','sales_for_this_month'];
+        return view('admin.home', compact($returnItem));
     }
 }
